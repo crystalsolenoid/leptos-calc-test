48784518b890a82812c07191038cc925e1b86d43
Quinten Konyn <qkonyn@gmail.com>
2025-07-04 19:38:10 -0700

    Add dynamic percentage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 #![feature(int_roundings)]
 
+use std::num::ParseIntError;
+
 use leptos::prelude::*;
 
 #[component]
 fn App() -> impl IntoView {
     let (quant, set_quant) = signal("".to_string());
-    let (value, set_value) = signal(10u64);
+    let (percent, set_percent) = signal(10u64);
+    let (custom_percent, set_custom_percent) = signal(false);
+
+    let quant_parsed = move || {
+        quant.get().parse::<u64>()
+    };
+
+    let over_thresh = move || {
+        quant_parsed()
+            .map(|q: u64| q > 500)
+    };
 
     let ten_percent = move || {
-        quant
-            .get()
-            .parse::<u64>()
-            // TODO this panics when q is too high
-            .map(|q: u64| q + (value.get() * q).div_ceil(100) + 1)
+        quant_parsed()
+            .map(|q: u64| q + (percent.get() * q).div_ceil(100) + 1)
     };
 
     view! {
@@ -20,28 +29,42 @@ fn App() -> impl IntoView {
             <input
                 id="quant"
                 type="number"
-                on:input:target=move |ev| { set_quant.set(ev.target().value()) }
+                on:input:target=move |ev| { 
+            set_quant.set(ev.target().value());
+            if !custom_percent.get() {
+            if over_thresh().unwrap_or(false) {set_percent.set(20)}
+                else {set_percent.set(10)}
+            };
+        }
                 prop:value=quant
             />
             " + "
-            <PercentSelector setter=set_value getter=value />
+            <PercentSelector setter=set_percent getter=percent custom=set_custom_percent thresh=Signal::derive(over_thresh)/>
             " + 1 = "
             <ErrorBoundary fallback=|errors| view! { <span class="error">"???"</span> }>
                 <strong>{ten_percent}</strong>
             </ErrorBoundary>
         </p>
         <p>
-            <button on:click=move |_| { set_quant.set("".to_string()) }>"clear"</button>
+            <button on:click=move |_| {
+                set_quant.set("".to_string());
+                set_custom_percent.set(false);
+                set_percent.set(10);
+            }>"clear"</button>
         </p>
     }
 }
 
 #[component]
-fn PercentSelector(setter: WriteSignal<u64>, getter: ReadSignal<u64>) -> impl IntoView {
+fn PercentSelector(setter: WriteSignal<u64>, getter: ReadSignal<u64>,
+    custom: WriteSignal<bool>,
+    #[prop(into)]
+    thresh: Signal<Result<bool, ParseIntError>>) -> impl IntoView {
     view! {
         <select
             on:change:target=move |ev| {
                 setter.set(ev.target().value().parse().unwrap());
+                custom.set(true);
             }
             prop:value=move || getter.get().to_string()
         >
